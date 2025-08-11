995e704857fcef1e18f3ad0d50105499d2b8c8ae
Vashishta Mithra Reddy Vundyala <vashishtamithrareddy@gmail.com>
2025-08-11 13:44:04 +0530

    header visibility fix

--- components/blocks/header.tsx
+++ components/blocks/header.tsx
@@ -5,7 +5,7 @@ import Navigation from "./navigation";
 
 export default function Header() {
 return (
-         <nav className="w-full max-w-6xl flex justify-center h-fit bg-foreground/5 backdrop-blur-3xl sticky top-0 md:top-8 rounded-none md:rounded-xl z-50 md:shadow-md">
+         <nav className="w-full max-w-6xl flex justify-center h-fit bg-gray-50/30 dark:bg-foreground/5 backdrop-blur-3xl sticky top-0 md:top-8 rounded-none md:rounded-xl z-50 md:shadow-md">
           <div className="w-full max-w-7xl flex justify-center items-center text-sm">
             {/* <div className="flex gap-5 items-center font-semibold"> */}
               <Link href={"/"} className="text-4xl py-4 text-start font-bold md:hidden flex text-muted-foreground">हित</Link>
