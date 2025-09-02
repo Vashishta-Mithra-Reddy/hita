b00c16f79556e1164448f77a5edcf25f5bf34752
Vashishta Mithra Reddy Vundyala <vashishtamithrareddy@gmail.com>
2025-09-02 13:53:29 +0530

    tags

--- app/layout.tsx
+++ app/layout.tsx
@@ -9,6 +9,7 @@ import Footer from "@/components/blocks/footer";
 import Header from "@/components/blocks/header";
 import BottomNav from "@/components/blocks/bottom-nav";
 import { Analytics } from "@vercel/analytics/next"
+import Script from "next/script";
 // import RadialGradient from "@/components/RadialGradient";
 // import BottomGradient from "@/components/BottomGradient";
 
@@ -42,7 +43,26 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" suppressHydrationWarning>
+      <head>
+        <Script id="gtm-script" strategy="afterInteractive">
+          {`(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+            })(window,document,'script','dataLayer','GTM-PQGRKC83');`}
+        </Script>
+      </head>
       <body className={`${geistSans.className} ${satoshi.variable} antialiased flex flex-col items-center`}>
+        
+        <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-PQGRKC83"
+            height="0"
+            width="0"
+            style={{ display: "none", visibility: "hidden" }}
+          ></iframe>
+        </noscript>
+
         <ThemeProvider
           attribute="class"
           defaultTheme="system"
