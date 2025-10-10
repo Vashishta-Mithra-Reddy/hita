406c93572964238723d11de3341460f474441cde
Vashishta Mithra Reddy Vundyala <vashishtamithrareddy@gmail.com>
2025-10-11 01:59:16 +0530

    fix

--- lib/supabase/middleware.ts
+++ lib/supabase/middleware.ts
@@ -51,7 +51,8 @@ export async function updateSession(request: NextRequest) {
     !request.nextUrl.pathname.startsWith("/brands") &&
     !request.nextUrl.pathname.startsWith("/foods") &&
     !request.nextUrl.pathname.startsWith("/login") &&
-    !request.nextUrl.pathname.startsWith("/auth")
+    !request.nextUrl.pathname.startsWith("/auth") &&
+    !request.nextUrl.pathname.startsWith("/agent")
   ) {
     // no user, potentially respond by redirecting the user to the login page
     const url = request.nextUrl.clone();
